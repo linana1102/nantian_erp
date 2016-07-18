59b5b6bf4f1f43778d15665557b6ac293b5f5a45
lbk0116 <mac@k16.local>
2016-07-18 15:36:11 +0800

    南天ERP系统 v0.1
    初步实现人力资源管理、项目管理的定制化修改。

---
+++ models/models.py
@@ -0,0 +1,148 @@
+﻿# -*- coding: utf-8 -*-
+from openerp import tools
+from openerp import models, fields, api
+from email.utils import formataddr
+import email,math
+from email.header import Header
+from datetime import datetime,timedelta
+
+class hr_employee(models.Model):
+    _inherit = 'hr.employee'
+
+    project_id = fields.Many2one('project.project',ondelete='set null',)
+    analytic_line_id = fields.Many2one('account.analytic.line',ondelete='set null',)
+    project_parent_id = fields.Many2one('account.analytic.account',compute='_compute_parent_project',store=True)
+    certificate_ids = fields.Many2many('nantian_erp.certificate',ondelete = 'set null',string="证书")
+    graduation = fields.Char(string="毕业学校")    
+    major = fields.Char(string='专业')
+    work_time = fields.Date()
+    entry_time = fields.Date()
+    contract_starttime = fields.Date()
+    contract_endtime = fields.Date(store=True,compute='_get_end_date')
+    contract_len = fields.Integer()
+    education = fields.Selection(
+        [
+            (u'专科', u"专科"),
+            (u'本科', u"本科"),
+            (u'硕士', u"硕士"),
+            (u'博士', u"博士"),
+            (u'专升本', u"专升本"),
+            (u'高级技工', u"高级技工"),
+            (u'高中', u"高中"),
+        ]
+
+    )
+    level = fields.Selection(
+        [
+            (u'1',1),
+            (u'2',2),
+            (u'3',3),
+            (u'4',4),
+            (u'5',5),
+            (u'6',6),
+            (u'7',7),
+            (u'8',8),
+            (u'9',9),
+        ]
+    )
+    category = fields.Selection(
+        [
+            (u'在公司', u"在公司"),
+            (u'在合同中', u"在合同中"),
+            (u'赠送', u"赠送"),
+            (u'开发', u"开发"),
+            (u'其他', u"其他"),
+        ],
+    default = u'在公司', string = "人员所属"
+    )
+    specialty = fields.Text(string="特长")
+
+    @api.depends('project_id')
+    def _compute_parent_project(self):
+        for record in self:
+            record.project_parent_id = record.project_id.parent_id
+    #@api.multi
+    #@api.onchange('project_id')
+    #def _onchange_analytic_line_id(self):
+        #self.analytic_line_id.name_get(self.project_id.analytic_account_id)
+       
+    @api.one
+    @api.depends('contract_starttime', 'contract_len')
+    def _get_end_date(self):
+        if not (self.contract_starttime and self.contract_len):
+            self.contract_endtime = self.contract_starttime
+            return
+        start=fields.Datetime.from_string(self.contract_starttime)
+        self.contract_endtime =datetime(start.year+self.contract_len,start.month,start.day)
+    
+class project_project(models.Model):
+    _inherit = 'project.project'
+
+    employee_ids = fields.One2many('hr.employee','project_id','Employee')
+    need_employee_count = fields.Integer()
+    employee_count = fields.Integer(compute='_count_employees', store=True)
+
+    @api.depends('employee_ids')
+    def _count_employees(self):
+        for employee in self.employee_ids:
+            self.message_subscribe([employee.user_id.partner_id.id])
+        for record in self:
+            record.employee_count = len(record.employee_ids)
+
+    @api.multi
+    @api.depends('name', 'need_employee_count')
+    def name_get(self):
+        return [(r.id, (r.name + '-' + u'所需人数' + (str(r.need_employee_count)) + u'人')) for r in self]
+
+class account_analytic_line(models.Model):
+    _inherit = 'account.analytic.line'
+
+    employee_ids = fields.One2many('hr.employee','analytic_line_id',"Employees")
+    employee_count = fields.Integer(compute='_count_employees',store=True)
+
+    @api.depends('employee_ids')
+    def _count_employees(self):
+        for record in self:
+            record.employee_count = len(record.employee_ids) 
+
+    @api.multi
+    @api.depends('name','product_id')
+    def name_get(self):
+       return [(r.id,(r.name+'('+(r.product_id.name)+')')) for r in self]
+
+class account_analytic_account(models.Model):
+    _inherit = 'account.analytic.account'
+
+    need_employee_count = fields.Integer(compute='_need_count_employees',store=True)
+
+    @api.depends('line_ids')
+    def _need_count_employees(self):
+        for record in self:
+            record.enee_mployee_count = 0
+            for line in self.line_ids:
+                record.need_employee_count += line.employee_count
+
+    @api.multi
+    @api.depends('name', 'employee_count')
+    def name_get(self):
+        return [(r.id, (r.name + '-'+u'所需人数'+  (str(r.need_employee_count)) +u'人')) for r in self]
+    
+class certificate(models.Model):
+    _name = 'nantian_erp.certificate'
+    _rec_name = 'name'
+
+    name = fields.Char(required=True,string='证书名')
+    category =fields.Selection(
+        [
+            (u'思科', u"思科"),
+            (u'华为', u"华为"),
+            (u'项目管理', u"项目管理"),
+            (u'红帽', u"红帽"),
+            ('juniper', "juniper"),
+            ('IBM', "IBM"),
+            ('Oracle', "Orcale"),
+
+        ],
+        string=u"证书类别"
+    )
+    employee_ids = fields.Many2many('hr.employee',ondelete='set null',)
